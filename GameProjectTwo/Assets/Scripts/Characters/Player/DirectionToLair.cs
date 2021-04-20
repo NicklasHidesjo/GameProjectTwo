1c11b5806fa56a87ecf496c1b000bf832fa539f0
Mellowtide-CH <stavlama@hotmail.com>
2021-04-20 11:44:30 +0200

    Lairfindingstuff with Axels Satiation

---
+++ GameProjectTwo/Assets/Scripts/Characters/Player/DirectionToLair.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionToLair : MonoBehaviour
+{
+    SpriteRenderer lairFinder;
+    Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lairFinder = gameObject.GetComponent<SpriteRenderer>();
+        target = GameObject.FindGameObjectWithTag("Lair").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf == true)
+        {
+            transform.LookAt(target);
+            transform.Rotate(90, 0, 0);
+        }
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
