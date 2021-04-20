02f18344c6fafc1250f2185fe68fe6140ab4a43b
Axel Eriksson <a.eriksson0303@gmail.com>
2021-04-20 17:20:49 +0200

    Generic NPCStatsManager to instansiate

---
+++ GameProjectTwo/Assets/Scripts/Characters/NPCStatsManager.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCStatsManager : StatsManager
+{
+	//För att instansiera  StatsManager 
+}
