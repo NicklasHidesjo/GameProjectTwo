eaa243314ca4df869ec5168c0d35b5eadadadd86
Robert Sandh <sarobban@gmail.com>
2021-04-22 13:19:02 +0200

    Update DestroyOnStart.cs

--- GameProjectTwo/Assets/Scripts/Camera/DestroyOnStart.cs
+++ GameProjectTwo/Assets/Scripts/Camera/DestroyOnStart.cs
@@ -7,7 +7,11 @@ public class DestroyOnStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit)){
+            transform.position = hit.point + Vector3.up;
+        }
         Renderer r = gameObject.GetComponent<Renderer>();
-        Destroy(r);
+       // Destroy(r);
     }
 }
