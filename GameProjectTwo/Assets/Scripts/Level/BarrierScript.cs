a73eae4880f03908c9deee0c79ea79203bef0184
Mellowtide-CH <stavlama@hotmail.com>
2021-04-26 11:26:32 +0200

    Customizable Level Barriers
    Add levels in the array via the editor.

---
+++ GameProjectTwo/Assets/Scripts/Level/BarrierScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierScript : MonoBehaviour
+{
+    int ActiveLevel;
+    [SerializeField] GameObject[] BarrierLevels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            LevelChange();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetAll();
+        }
+    }
+    
+    void LevelChange()
+    {
+        BarrierLevels[ActiveLevel].SetActive(false);
+        ActiveLevel++;
+    }
+
+    void ResetAll()
+    {
+        for (int i = 0; i < BarrierLevels.Length; i++)
+        {
+            BarrierLevels[i].SetActive(true);
+        }
+    }
+}
