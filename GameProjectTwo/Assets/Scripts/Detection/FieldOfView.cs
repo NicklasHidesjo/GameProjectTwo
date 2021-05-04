044b008156f2b608619a17def965b15d2c5f0840
Robert Sandh <sarobban@gmail.com>
2021-05-04 14:48:38 +0200

    Update FieldOfView.cs

--- GameProjectTwo/Assets/Scripts/Detection/FieldOfView.cs
+++ GameProjectTwo/Assets/Scripts/Detection/FieldOfView.cs
@@ -4,8 +4,14 @@ using System.Collections.Generic;
 
 public class FieldOfView : MonoBehaviour
 {
-	[SerializeField] LayerMask targetMask;
-	[SerializeField] LayerMask obstacleMask;
+    [SerializeField] LayerMask targetMask;
+    [SerializeField] LayerMask obstacleMask;
+
+    [Header("ConeCastFromToHeads")]
+    [SerializeField] int segmentsInCone = 4;
+    [SerializeField] float plHeadRadius = 0.45f;
+    [SerializeField] float plNeckHight = 0.5f;
+    [SerializeField] float nPCNeckHight = 0.5f;
 
     float deathTimer;
 
@@ -68,17 +74,17 @@ public class FieldOfView : MonoBehaviour
     }
     void FindVisibleTargets()
     {
-        if(npc.IsDead) { return; }
+        if (npc.IsDead) { return; }
         Collider[] playersDetected = Physics.OverlapSphere(transform.position, npc.Stats.SightLenght, targetMask);
 
         npc.NoticedPlayer = false;
         npc.SeesPlayer = false;
 
-        if (playersDetected.Length < 1) 
+        if (playersDetected.Length < 1)
         {
             npc.SawHiding = false;
             npc.SawTransforming = false;
-            return; 
+            return;
         }
 
         foreach (var player in playersDetected)
@@ -96,22 +102,29 @@ public class FieldOfView : MonoBehaviour
                 }
                 continue;
             }
-            if(playerState == PlayerState.playerStates.BatDefault)
-			{
-                if(npc.SawTransforming)
-				{
+            if (playerState == PlayerState.playerStates.BatDefault)
+            {
+                if (npc.SawTransforming)
+                {
                     npc.RaiseAlertness(true);
                     npc.SeesPlayer = true;
                     npc.NoticedPlayer = true;
                     npc.TimeSinceLastSeenPlayer = 0;
                 }
                 continue;
-			}
+            }
 
             Vector3 dirToTarget = (player.transform.position - transform.position).normalized;
             RaycastHit hit;
             if (Vector3.Angle(transform.forward, dirToTarget) < npc.FOV / 2)
             {
+                //Robert was here!
+                bool seesPlayer = ConeCast(transform.position, player.transform.position, npc.Stats.SightLenght);
+                if (!seesPlayer)
+                {
+                    return;
+                }
+                /*
                 if (!Physics.Raycast(transform.position, dirToTarget, out hit, npc.Stats.SightLenght))
                 {
                     return;
@@ -120,7 +133,7 @@ public class FieldOfView : MonoBehaviour
                 {
                     return;
                 }
-
+                */
                 npc.SeesPlayer = true;
                 npc.RaiseAlertness(true);
                 npc.TimeSinceLastSeenPlayer = 0;
@@ -134,9 +147,9 @@ public class FieldOfView : MonoBehaviour
                     npc.SawHiding = false;
                 }
                 if (playerState == PlayerState.playerStates.TransformToBat)
-				{
+                {
                     npc.SawTransforming = true;
-				}
+                }
                 else if (playerState != PlayerState.playerStates.BatDefault)
                 {
                     npc.SawTransforming = false;
@@ -164,5 +177,41 @@ public class FieldOfView : MonoBehaviour
                 npc.TimeSinceLastSeenPlayer = 0;
             }
         }
+    }
+
+    bool ConeCast(Vector3 from, Vector3 to, float rayLength)
+    {
+
+        Vector3 fromToEye = from + Vector3.up * nPCNeckHight;
+        Vector3 toToHead = to + Vector3.up * plNeckHight;
+        Vector3 dir = fromToEye - toToHead;
+        
+        float radius = plHeadRadius;
+        int numberOfRays = segmentsInCone;
+        float angStep = 360 / segmentsInCone;
+
+        RaycastHit hit;
+        for (int i = 0; i < numberOfRays; i++)
+        {
+            Vector3 offDir = Quaternion.LookRotation(dir) * Quaternion.Euler(0, 0, angStep * i) * Vector3.up * radius;
+            Debug.DrawLine(fromToEye, toToHead + offDir, Color.magenta);
+
+            if (Physics.Linecast(fromToEye, toToHead + offDir, out hit))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    Debug.DrawRay(fromToEye, toToHead + offDir, Color.red);
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    IEnumerator debug(Vector3 from, Vector3 offDir, float l)
+    {
+        yield return new WaitForFixedUpdate();
+        Debug.DrawRay(from, offDir, Color.red);
     }
 }
