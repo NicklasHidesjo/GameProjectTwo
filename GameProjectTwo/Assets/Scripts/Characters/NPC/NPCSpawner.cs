3220c8aa260599c6cf58fac25729c1f53c8b5124
Axel Eriksson <a.eriksson0303@gmail.com>
2021-05-05 15:28:19 +0200

    Revert "Progress on NPC spawner"
    This reverts commit af67ad8d69a99c81b45266839f66b20de9e82eec.

--- GameProjectTwo/Assets/Scripts/Characters/NPC/NPCSpawner.cs
+++ GameProjectTwo/Assets/Scripts/Characters/NPC/NPCSpawner.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class NPCSpawner : MonoBehaviour
 {
+	private DistrictManager districtManager;
+	private EndLevelCheck endLevelCheck;
 	[SerializeField] GameObject civilian;
 	[SerializeField] GameObject guard;
 	[SerializeField] Transform[] spawnLocations;
-	
-	private int civiliansActive;
-	private int guardsActive;
+
+	[SerializeField] private GameObject[] districts;
+	int civiliansActive;
 
 	[SerializeField] private int civilianPoolSize = 5;
 	[SerializeField] private int guardPoolSize = 5;
@@ -30,20 +32,12 @@ public class NPCSpawner : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+		
+		endLevelCheck = GameObject.FindGameObjectWithTag("Lair").GetComponent<EndLevelCheck>();
 		NpcPoolManager.Instance.CreatePool(civilian, civilianPoolSize);
 		NpcPoolManager.Instance.CreatePool(guard, guardPoolSize);
-
-		for (int i = 0; i < civilianPoolSize; i++)
-		{
-			NpcSpawn(true);
-		}
-
-		for (int i = 0; i < guardPoolSize; i++)
-		{
-			NpcSpawn(false);
-		}
-		//StartCoroutine(NpcSpawnEnumerator(true, 3f));
-
+		
+		//StartCoroutine(CivilianSpawn());
 		
 	}
 
@@ -52,74 +46,45 @@ public class NPCSpawner : MonoBehaviour
 	{
 
 		//All IF-statements below in Update are for testing purposes
+
+		//if (civiliansActive < (endLevelCheck.LevelPassedThreshold[endLevelCheck.CurrentLevel] / 100) * 2)
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			//StartCoroutine(CivilianSpawn());
+			//NpcSpawnCivilian(civilian, districts[1]);
+		}
+	}
+
+	IEnumerator CivilianSpawn()
+	{
+		int i = Random.Range(0, spawnLocations.Length);
+		Transform currentSpawn = spawnLocations[i];
+		NpcPoolManager.Instance.ReuseNpc(civilian, currentSpawn);
+		yield return new WaitForSeconds(3.0f);
+
 		if (civiliansActive < civilianPoolSize)
 		{
-			//StartCoroutine(NpcSpawnEnumerator(true, 3f));
-			NpcSpawn(true);
-		}
-
-		if (guardsActive < guardPoolSize)
-		{
-			NpcSpawn(false);
-		}
-
-		if (Input.GetKeyDown(KeyCode.G))
-		{
-			NpcDespawn(false, GameObject.FindGameObjectWithTag("Guard"));
-		}
-
-		if (Input.GetKeyDown(KeyCode.C))
-		{
-			NpcDespawn(true,GameObject.FindGameObjectWithTag("Civilian"));
-		}
-	}
-
-	IEnumerator NpcSpawnEnumerator(bool isCivilian, float delayTime)
-	{
-		Debug.Log("Spawn Enum going");
-		yield return new WaitForSeconds(delayTime);
-		int i = Random.Range(0, spawnLocations.Length);
-		Transform currentSpawn = spawnLocations[i];
-		if (isCivilian && civiliansActive < civilianPoolSize)
-		{
-			NpcPoolManager.Instance.ReuseNpc(civilian, currentSpawn);
 			civiliansActive++;
 		}
-		else if (guardsActive < guardPoolSize)
-		{
-			NpcPoolManager.Instance.ReuseNpc(guard, currentSpawn);
-			guardsActive++;
-		}
 	}
 
-
-	private void NpcSpawn(bool isCivilian)
+	public void NpcSpawn(bool isCivilian, Transform[] districtSpawnPoints)
 	{
-		int i = Random.Range(0, spawnLocations.Length);
-		Transform currentSpawn = spawnLocations[i];
+		int i = Random.Range(0, districtSpawnPoints.Length);
+		Transform currentSpawn = districtSpawnPoints[i];
 		if (isCivilian)
 		{
 			NpcPoolManager.Instance.ReuseNpc(civilian, currentSpawn);
-			civiliansActive++;
 		}
 		else
 		{
 			NpcPoolManager.Instance.ReuseNpc(guard, currentSpawn);
-			guardsActive++;
 		}
 	}
 
-	public void NpcDespawn(bool isCivilian, GameObject npc)
+	public void NpcDespawn(GameObject npc, GameObject district)
 	{
 		npc.SetActive(false);
-
-		if (isCivilian)
-		{
-			civiliansActive--;
-		}
-		else
-		{
-			guardsActive--;
-		}
+		
 	}
 }
