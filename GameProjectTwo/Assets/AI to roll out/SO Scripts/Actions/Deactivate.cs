9082f2ed8db6e94fc58d059efa48ba29a1467a3e
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-05-11 14:38:57 +0200

    small tweak
    stationary civilians who have died will now no longer respawn in position.

--- GameProjectTwo/Assets/AI to roll out/SO Scripts/Actions/Deactivate.cs
+++ GameProjectTwo/Assets/AI to roll out/SO Scripts/Actions/Deactivate.cs
@@ -5,14 +5,23 @@ public class Deactivate : Action
 {
 	public override void Execute(ICharacter character)
 	{
-		if(character.Self.CompareTag("Guard"))
+		if (character.Self.CompareTag("Guard"))
 		{
 			NPCSpawner.Instance.NpcDespawn(false, character.Self);
 		}
 		else
 		{
 			NPCSpawner.Instance.NpcDespawn(true, character.Self);
-		}
 
+			if (!character.Stationary)
+			{
+				return;
+			}
+			if (!character.IsDead)
+			{
+				return;
+			}
+			NPCSpawner.Instance.RemoveStationaryNPC(character.Self);
+		}
 	}
 }
