ab113227756b12a1c6f8b42428cacd904deb9bce
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-05-14 09:54:44 +0200

    Update SpawnPath.cs

--- GameProjectTwo/Assets/Scripts/Characters/Level/SpawnPath.cs
+++ GameProjectTwo/Assets/Scripts/Characters/Level/SpawnPath.cs
@@ -27,7 +27,25 @@ public class SpawnPath : MonoBehaviour
     {
         List<PathPoint> path = new List<PathPoint>();
 
-        //Randomize a point in each spawn area to create a path
+        List<PointGenerator> points = GetComponentsInChildren<PointGenerator>().ToList();
+
+		foreach (var point in points)
+		{
+            Vector3 pos = point.transform.position;
+            Vector3 bounds = point.GetComponent<Renderer>().bounds.extents;
+            pos += new Vector3(Mathf.Lerp(-bounds.x, bounds.x, Random.value), 0, Mathf.Lerp(-bounds.z, bounds.z, Random.value));
+
+            RaycastHit hit;
+            if (Physics.Raycast(pos + Vector3.up * 100, Vector3.down, out hit, 200, checkLayers, QueryTriggerInteraction.Ignore))
+            {
+                pos.y = hit.point.y + 1;
+            }
+
+            path.Add(new PathPoint(pos, point.IdlePoint));
+        }
+
+
+/*        //Randomize a point in each spawn area to create a path
         for (int i = 0; i < transform.childCount; i++)
         {
             Vector3 pos = transform.GetChild(i).position;
@@ -40,14 +58,14 @@ public class SpawnPath : MonoBehaviour
                 pos.y = hit.point.y + 1;
             }
 
-            path.Add(new PathPoint(pos, transform.GetChild(i).GetComponent<PointGenerator>().IdlePoint));
-        }
+            points.Add(new PathPoint(pos, transform.GetChild(i).GetComponent<PointGenerator>().IdlePoint));
+        }*/
 
         if (flippedPath)
         {
                 PathPoint[] newPath = new PathPoint[path.Count];
                 int y = path.Count - 1;
-                for (int x = 0; x < path.Count; x++)
+                for (int x = 0; x < points.Count; x++)
                 {
                     newPath[y] = path[x];
                     y--;
